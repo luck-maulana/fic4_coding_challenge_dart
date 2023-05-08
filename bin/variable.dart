24ece39695ddd1e24b41b397f799a793394fed00
luck-maulana <luckymaulana35@gmail.com>
2023-05-09 03:24:24 +0700

    add variable.dart

---
+++ bin/variable.dart
@@ -0,0 +1,10 @@
+void main() {
+  double p = 12;
+  double l = 10.4;
+  double luas = p * l;
+
+  print('Luas Persegi Panjang');
+  print('Panjang : $p');
+  print('Lebar : $l');
+  print('Luas = $luas');
+}
