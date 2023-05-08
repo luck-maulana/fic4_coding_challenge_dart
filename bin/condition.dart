d460b3281fb58c507074e41cc90ec351670b5278
luck-maulana <luckymaulana35@gmail.com>
2023-05-09 03:34:38 +0700

    add condition.dart

---
+++ bin/condition.dart
@@ -0,0 +1,11 @@
+void main() {
+  int angka = -1;
+
+  if (angka > 0) {
+    print('Angka tersebut positif');
+  } else if (angka < 0) {
+    print('Angka tersebut negatif');
+  } else {
+    print('Angka tersebut nol');
+  }
+}
